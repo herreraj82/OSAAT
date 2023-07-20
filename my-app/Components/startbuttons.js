31663d826e0cc2a53a31e8ddc65f191f757d69da
herreraj82 <herrera.j82@gmail.com>
2023-07-20 16:46:37 -0400

    Added delete book functionality

--- my-app/Components/startbuttons.js
+++ my-app/Components/startbuttons.js
@@ -1,16 +1,13 @@
 import {
     View,
-    Text,
     Button,
-    StatusBar,
-    TouchableWithoutFeedback,
-    TextInput,
     Dimensions,
+    Pressable,
+    Text
   }                          from "react-native";
   import { useState }        from "react";
   import * as FileSystem     from "expo-file-system";
   import { useEffect } from "react";
-  import * as DocumentPicker from 'expo-document-picker';
 
   
   export default function StartButtons(props) {
@@ -31,11 +28,44 @@ import {
         return ()=>{};
     },[])
 
+    async function deleteBook(idToDelete, sentencesUriToDelete, saveUriToDelete) {
+        const fileTable_uri = FileSystem.documentDirectory + 'filetable.txt';
+
+        const file_contents = await FileSystem.readAsStringAsync(fileTable_uri);
+
+        let tempFileTable = JSON.parse(file_contents);
+        tempFileTable = tempFileTable.filter(elt => elt.identifier !== idToDelete);
+        setFileTable(tempFileTable);
+
+        FileSystem.writeAsStringAsync(FileSystem.documentDirectory+'filetable.txt',JSON.stringify(tempFileTable));
+        FileSystem.deleteAsync(sentencesUriToDelete);
+        FileSystem.deleteAsync(saveUriToDelete);
+        return;
+    }
+
     return (
         <View style={{width:"100%", minHeight:Dimensions.get('window').height/2,flexDirection:'column',justifyContent:'space-around'}}>
             <Button style={{width:"100%"}} title="Convert Ebook" onPress={props.osaat} />
             {fileTable && fileTable.map((elt) => {
-                return (<Button style={{width:"100%"}} title={elt.title} key={elt.title} onPress={() => props.openConvertedBook(elt.sentences_uri, elt.save_uri, elt.toc)} />)
+                return (<Pressable 
+                            style={{
+                                
+                                width:"100%",
+                                backgroundColor:"blue",
+                                fontSize:15,
+                                height:50,
+                                justifyContent:"center",
+                                alignItems: "center"
+                            }} 
+                            title={elt.title} 
+                            key={elt.title} 
+                            onPress={() => props.openConvertedBook(elt.sentences_uri, elt.save_uri, elt.toc)} 
+                            onLongPress={()=>{deleteBook(elt.identifier, elt.sentences_uri, elt.save_uri)}} 
+                        >
+                            <Text style={{
+                                color:"cornsilk"
+                            }}>{elt.title}</Text>
+                        </Pressable>)
             })}
         </View>
     )
